7f26f773287c582a4c765a2d7c2adc63bd6b1821
Melissa <m.giramata@alustudent.com>
2023-11-29 21:32:30 +0200

    p

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tensorflow as tf
+import tensorflow 
 
 # Load pre-trained model
-model = tf.keras.models.load_model('model.h5')
+model = tensorflow.keras.models.load_model('model.h5')
 
 # Function to preprocess the image for the model
 def preprocess_image(image):
@@ -23,14 +23,18 @@ def predict_metal(image):
 def main():
     st.title("Metal Detection App")
 
-    # Provide the path to the image directly
-    image_path = "path/to/your/image.jpg"  # Replace with the actual path to your image file
+    uploaded_file = st.file_uploader("Upload an image for metal detection", type=["jpg", "jpeg", "png"])
 
-    st.image(image_path, caption="Uploaded Image", use_column_width=True)
+    if uploaded_file is not None:
+        image_path = f"uploads/{uploaded_file.name}"
+        with open(image_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
 
-    if st.button("Click here to Predict"):
-        prediction = predict_metal(image_path)
-        st.write(f"Prediction Result: {prediction}")
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+        if st.button("Click here to Predict"):
+            prediction = predict_metal(image_path)
+            st.write(f"Prediction Result: {prediction}")
 
 if __name__ == "__main__":
     main()
